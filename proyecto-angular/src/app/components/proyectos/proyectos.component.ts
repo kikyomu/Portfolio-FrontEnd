30d484a763c06abfff6abd73fd0f4c1fbd88714b
kikyomu <kikyomuok@gmail.com>
2023-05-07 21:19:27 -0300

    Creación de controladores e implementacion de data binding en el componente proyectos

--- proyecto-angular/src/app/components/proyectos/proyectos.component.ts
+++ proyecto-angular/src/app/components/proyectos/proyectos.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
 
 @Component({
   selector: 'app-proyectos',
   templateUrl: './proyectos.component.html',
   styleUrls: ['./proyectos.component.css']
 })
-export class ProyectosComponent {
+export class ProyectosComponent implements OnInit {
 
+  proyectosList: any;
+
+  constructor(private datosPortfolio:PortfolioService) { }
+  
+  ngOnInit(): void {
+    this.datosPortfolio.obtenerDatos().subscribe(data =>{
+      console.log(data);
+      this.proyectosList=data.proyectos;
+    });
+  }
+  
 }
