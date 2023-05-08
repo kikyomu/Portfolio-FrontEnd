fe2efe20921a01207e2c08171d5615070e28bbc1
kikyomu <kikyomuok@gmail.com>
2023-05-07 21:22:03 -0300

    Creación de controladores e implementacion de data binding en los componentes: skills-herramientas, skills-back-end y sobre-mi

--- proyecto-angular/src/app/components/skills-back-end/skills-back-end.component.ts
+++ proyecto-angular/src/app/components/skills-back-end/skills-back-end.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
 
 @Component({
   selector: 'app-skills-back-end',
   templateUrl: './skills-back-end.component.html',
   styleUrls: ['./skills-back-end.component.css']
 })
-export class SkillsBackEndComponent {
+export class SkillsBackEndComponent implements OnInit {
+
+  skillsBackList: any;
+
+  constructor(private datosPortfolio:PortfolioService) { }
+  
+  ngOnInit(): void {
+    this.datosPortfolio.obtenerDatos().subscribe(data =>{
+      this.skillsBackList=data.skills['back-end'];
+    })
+  }
 
 }
+
